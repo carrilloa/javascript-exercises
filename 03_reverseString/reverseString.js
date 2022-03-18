18ce5bda6137eb63010d306ad457df9c6a0c1fcb
carrilloa <annasofiacarrillo42@gmail.com>
2022-03-18 20:53:05 +0100

    finished the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let x = string;
+    let splitString = string.split("");
+    x = splitString.reverse();
+    x = x.join("");
+    return x;
 };
 
 // Do not edit below this line
