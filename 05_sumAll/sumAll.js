da960e47ce66818298997a36deea4de3f543a05b
carrilloa <annasofiacarrillo42@gmail.com>
2022-03-23 14:52:28 +0100

    finished exercise on first try
    git statues
    git status
    clear
    cd

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
+    let toAdd = 0;
+    let until = 0;
+    if (firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
+    else if (typeof(firstNumber) !== "number" || typeof(secondNumber) !== "number") {
+        return "ERROR"
+    }
+    else if (firstNumber > secondNumber) {
+        toAdd = secondNumber;
+        until = firstNumber;
+    }
+    else if (secondNumber > firstNumber) {
+        toAdd = firstNumber;
+        until = secondNumber;
+    }
+    for (i = 0; i < until; i++) {
+        sum += toAdd;
+        toAdd++;
+    }
+    return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
