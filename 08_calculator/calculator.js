6eff61b339a493de4e7613c84e81def9ba427e98
carrilloa <annasofiacarrillo42@gmail.com>
2022-03-24 15:29:49 +0100

    completed exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(numberOne, numberTwo) {
+    return numberOne + numberTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numberOne, numberTwo) {
+    return numberOne - numberTwo;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (i = array.length - 1; i >= 0; i--) {
+    sum += array[i];
+  }
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (i = array.length - 1; i >= 0; i--) {
+    product *= array[i];
+  }
+	return product; 
 };
 
-const power = function() {
-	
+const power = function(numberOne, numberTwo) {
+	let answer = 1;
+  for (i = 0; i < numberTwo; i++) {
+    answer *= numberOne;
+  }
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let answer = 1;
+  if (number == 0) {
+    return 1;
+  }
+  for (i = 0; i < number;) {
+    answer *= number;
+    number--;
+  }
+	return answer;
 };
 
 // Do not edit below this line
