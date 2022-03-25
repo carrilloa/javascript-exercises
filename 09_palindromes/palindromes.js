cdb78e0e7982902d0f0c66ee9a2eb392187c6bdd
carrilloa <annasofiacarrillo42@gmail.com>
2022-03-25 23:08:09 +0100

    finished the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alphabet = [
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+        "w", "x", "y", "z"
+    ]
+    let newString = string.toLowerCase();
+    let forwardsArray = []
+    let backwardsArray = []
+    for (i = 0; i < newString.length; i++) {
+        if (alphabet.includes(newString[i])) {
+            forwardsArray.push(newString[i]);
+            backwardsArray.push(newString[i]);
+        }
+    }
+    backwardsArray.reverse()
+    for (i = 0; i < forwardsArray.length; i++) {
+        if (forwardsArray[i] == backwardsArray[i]) {
+            continue;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
