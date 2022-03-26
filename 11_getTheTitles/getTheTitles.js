6ac1141b56949b32749dd7cff882cdad601b12d3
carrilloa <annasofiacarrillo42@gmail.com>
2022-03-26 11:35:22 +0100

    finished the exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    arrayOne = Array.from(books);
+    mapOne = arrayOne.map(({title}) => title);
+    return mapOne;
 };
 
 // Do not edit below this line
