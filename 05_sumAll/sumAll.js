291b1185701fc1bd060777dc986d2c43ff673dee
carrilloa <annasofiacarrillo42@gmail.com>
2022-04-04 21:40:42 +0200

    second try

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,24 +1,18 @@
 const sumAll = function(firstNumber, secondNumber) {
     let sum = 0;
-    let toAdd = 0;
-    let until = 0;
     if (firstNumber < 0 || secondNumber < 0) {
-        return "ERROR";
-    }
-    else if (typeof(firstNumber) !== "number" || typeof(secondNumber) !== "number") {
         return "ERROR"
     }
-    else if (firstNumber > secondNumber) {
-        toAdd = secondNumber;
-        until = firstNumber;
+    if (firstNumber > secondNumber) {
+        let temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
     }
-    else if (secondNumber > firstNumber) {
-        toAdd = firstNumber;
-        until = secondNumber;
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
+        return "ERROR"
     }
-    for (i = 0; i < until; i++) {
-        sum += toAdd;
-        toAdd++;
+    for (i = firstNumber; i < secondNumber + 1; i++) {
+        sum += i;
     }
     return sum;
 }
