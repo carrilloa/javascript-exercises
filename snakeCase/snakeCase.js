be2c584649f7562aef71d0f25edab5b4e9bbdd97
carrilloa <annasofiacarrillo42@gmail.com>
2022-04-08 11:12:23 +0200

    Started to try this exercise

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,6 +1,13 @@
-const snakeCase = function() {
-
-};
+const snakeCase = function(string) {
+    let newArray = Array.from(string).replace(/[^a-z]/g, "");
+    let result = [];
+    newArray.map(character => {
+        if (character == " ")
+        if (character !== character.toUpperCase()) {
+            result.push(character)
+        }
+    
+});
 
 // Do not edit below this line
 module.exports = snakeCase;
