897284adb366aa3c0791496dae20c9b5c0090999
carrilloa <annasofiacarrillo42@gmail.com>
2022-04-25 12:29:27 +0200

    second try

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,28 +1,11 @@
 const palindromes = function (string) {
-    const alphabet = [
-        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-        "w", "x", "y", "z"
-    ]
-    let newString = string.toLowerCase();
-    let forwardsArray = []
-    let backwardsArray = []
-    for (i = 0; i < newString.length; i++) {
-        if (alphabet.includes(newString[i])) {
-            forwardsArray.push(newString[i]);
-            backwardsArray.push(newString[i]);
-        }
-    }
-    backwardsArray.reverse()
-    for (i = 0; i < forwardsArray.length; i++) {
-        if (forwardsArray[i] == backwardsArray[i]) {
-            continue;
-        }
-        else {
-            return false;
-        }
-    }
-    return true;
+    processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (
+        processedString
+            .split("")
+            .reverse()
+          .join("") ==processedString
+    );
 };
 
 // Do not edit below this line
